1d2e1d4052a9fee9892406d3e4b67a06f377c000
fuadhasan199 <fuadhasan740266@gmail.com>
2026-02-21 23:48:52 +0600

    view details api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors') 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = 3000 
@@ -27,10 +27,45 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 }) 
 
+
+
 async function run() {
-  try {
+  try {  
+ await client.connect();
+
+ const BoighorDB = client.db('BoighorDB')
+ const BoighorCollection =BoighorDB.collection('BoighorCollection') 
+
+ 
+ app.post('/books',async(req,res)=>{
+     const book=req.body 
+     const result= await BoighorCollection.insertOne(book)
+     res.send(result)
+ }) 
+
+ app.get('/books',async(req,res)=>{ 
+   const query=req.query
+   const result=await BoighorCollection.find().toArray()
+   res.send(result)
+ }) 
+
+ app.get('/books/:id',async(req,res)=>{
+   const id=req.params.id 
+   const query={_id:new ObjectId(id)}
+   const result=await BoighorCollection.findOne(query)
+    res.send(result)
+ })
+
+
+
+
+
+
+
+
+
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
